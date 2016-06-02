32b5ba0156da838d1888a0735b54135327190d4f
Brandon Tassone <brandontassone@gmail.com>
2016-06-02 04:16:36 -0400

    Initial Plugin Commit

---
+++ ld-iet.php
@@ -0,0 +1,12 @@
+<?php
+/*
+	Plugin Name: LearnDash Import/Export Tool
+	Plugin URI: http://corduroybeach.com
+	Description: Imports / Exports Course and Other Data
+	Author: Brandon Tassone
+	Version: 0.1
+	Author URI: http://brandont.me
+*/
+
+// Composer Autoload Files
+require_once("vendor/autoload.php");
