7720502ac84d31d44e67e0f4cb1ce777fd35da85
Brandon Tassone <brandontassone@gmail.com>
2016-06-03 02:29:04 -0400

    Added gulpfile.js for task running

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+// Gulpfile for javascript tasks and watchers
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('styles', function(){
+    gulp.src('./resources/scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./resources/css/'));
+});
+
+gulp.task('default',function() {
+    gulp.watch('./resources/scss/**/*.scss',['styles']);
+});
